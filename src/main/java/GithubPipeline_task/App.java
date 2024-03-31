2b838115ae85b1a40f446141d0ab350209dbd028
YuvaDaraneePrabakar <164655855+YuvaDaraneePrabakar@users.noreply.github.com>
2024-03-31 10:04:20 +0530

    Add files via upload

---
+++ src/main/java/GithubPipeline_task/App.java
@@ -0,0 +1,9 @@
+package GithubPipeline_task;
+public class App {
+	
+	public static void main(String args[])
+	{
+		System.out.println("Maven_Application_Started");
+	}
+
+}
---
+++ src/test/java/GithubPipeline_task/AppTest.java
@@ -0,0 +1,49 @@
+package GithubPipeline_task;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+    @Test
+    public void apptest() {
+    	
+        String expectedOutput = "Maven_Application_Started";
+        
+        App.main(null);
+        
+        String consoleOutput = getConsoleOutput();
+        
+        Assert.assertEquals(consoleOutput.trim(), expectedOutput);
+    }
+    
+    private String getConsoleOutput() {
+    	
+        java.io.ByteArrayOutputStream out = new java.io.ByteArrayOutputStream();
+        java.io.PrintStream printStream = new java.io.PrintStream(out);
+        System.setOut(printStream);
+        
+        App.main(null);
+
+        System.setOut(System.out);
+
+        return out.toString();
+    }
+}
+
+/*
+package GithubPipeline_task;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class AppTest {
+
+    @Test
+    public void testprint() 
+    {
+    	String message = "God is great";
+    	Assert.assertEquals(message, "God is great", "Message should be 'God is great'");
+    }
+}
+*/
